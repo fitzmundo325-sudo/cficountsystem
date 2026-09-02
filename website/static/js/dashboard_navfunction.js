c7fd87a20501f5c0e3b5516e31e7d18f3fd9dfeb
ItsAtomTech <121592054+ItsAtomTech@users.noreply.github.com>
2026-09-02 14:40:31 +0800

    Added the Supply Management to v2 dashboard

--- website/static/js/dashboard_navfunction.js
+++ website/static/js/dashboard_navfunction.js
@@ -101,6 +101,16 @@ function showStoresTargetPageAdmin(elm){
 
 
 
+function showSupplyMangPageAdmin(elm){
+	activate(elm);
+
+	let page = open_modal("supply_v2", 'modal_on_container,no_close_button,page_containment', _('general_container'),false, undefined, true);
+	closeAllPages(page,false);
+	hideDashboardContents(true);
+	
+	
+}
+
 
 
 
